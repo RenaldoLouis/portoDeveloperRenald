ffcf60328d1c994ae3ed45f0026f8028c9e59845
RenaldoLouis <renald@10xers.co>
2023-10-11 21:32:26 +0700

    init git

---
+++ src/Pages/Home/Content.js
@@ -0,0 +1,46 @@
+import React from "react";
+import homeCard from "../../assets/images/spiderman.jpg"
+
+const Content = () => {
+
+    return (
+        <>
+            <img className="home-coverImage" src={homeCard} alt="Italian Trulli" />
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        1 of 2
+                    </div>
+                    <div class="col">
+                        2 of 2
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        1 of 3
+                    </div>
+                    <div class="col">
+                        2 of 3
+                    </div>
+                    <div class="col">
+                        3 of 3
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col align-self-start">
+                        One of three columns
+                    </div>
+                    <div class="col align-self-center">
+                        One of three columns
+                    </div>
+                    <div class="col align-self-end">
+                        One of three columns
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Content;
