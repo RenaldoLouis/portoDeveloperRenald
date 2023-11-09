7e02fc8258c28326210d7a72888f629e9742cdad
RenaldoLouis <renald@10xers.co>
2023-11-09 09:38:44 +0700

    finish experience page

--- src/Pages/Home/Experience.js
+++ src/Pages/Home/Experience.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Experience = () => {
-
+    const experienceData = [
+        {
+            year: "2020",
+            title: "Software Developer",
+            company: "BNI Life",
+            role: "internship",
+            status: "done"
+        },
+        {
+            year: "2020",
+            title: "Software Developer",
+            company: "Pro-Int Dinamika",
+            role: "internship",
+            status: "done"
+        },
+        {
+            year: "2020",
+            title: "Software Developer",
+            company: "10XERS Company",
+            role: "Part Time",
+            status: "done"
+        },
+        {
+            year: "2020",
+            title: "Software Developer",
+            company: "Commsult",
+            role: "Full Time",
+            status: "current"
+        }
+    ]
     return (
         <div>
             <div className="sectionTitleContainer">
@@ -10,39 +39,31 @@ const Experience = () => {
                 </span>
             </div>
 
-            <div className="container" style={{ paddingBottom: 25 }}>
+            <div className="container" style={{ paddingBottom: 300 }}>
                 <div class='line-container'>
                     <div class='progress-line'>
-                        <div class='progress' style={{ width: "50%" }}>
-
-                        </div>
-                        <div class='status'>
-                            <div class='dot completed'>
-
+                        {/* <div class='progress' style={{ width: "50%" }}/> */}
+                        {experienceData.map((eachExperience) => (
+                            <div key={eachExperience.title} class='status'>
+                                <div class={`dot ${eachExperience.status === "current" ? "current" : ""}`}>
+                                    <div style={{ marginTop: 64 }}>
+                                        <div>
+                                            {eachExperience.year}
+                                        </div>
+                                        <div className="titleExperience">
+                                            {eachExperience.title}
+                                        </div>
+                                        <div className="experienceDetails">
+                                            <div class="text-with-circle"> {eachExperience.company}<span class="circle"></span></div>
+                                            <div style={{ marginLeft: 19, textWrap: "nowrap" }}>
+                                                {eachExperience.role}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class='status'>
-                            <div class='dot completed'>
-
-                            </div>
-                        </div>
-                        <div class='status'>
-                            <div class='dot current'>
-
-                            </div>
-                        </div>
-                        <div class='status'>
-                            <div class='dot'>
-
-                            </div>
-                        </div>
-                        <div class='status'>
-                            <div class='dot'>
-
-                            </div>
-                        </div>
+                        ))}
                     </div>
-
                 </div>
             </div>
         </div>
