fe8ddc82fbef240d380b4647a4b336a421557373
RenaldoLouis <renald@10xers.co>
2023-11-11 23:42:22 +0700

    prepaore lauput footer

--- src/components/molecules/Footer.js
+++ src/components/molecules/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import linkedIn from '../../assets/images/cib_linkedin-in.svg';
+import instagram from '../../assets/images/ri_instagram-fill.svg';
+import github from '../../assets/images/teenyicons_github-solid.svg';
 
 const Footer = () => {
 
@@ -6,34 +9,39 @@ const Footer = () => {
         <div className="footer">
             <div class="container">
                 <div class="row">
-                    <div class="col">
-                        1 of 2
+                    <div class="col-7" style={{ textAlign: "start" }}>
+                        Contact
+                        Online
+                        <div >
+                            Discuss a project for just want to say hi? My inbox is open for all
+                        </div>
+                        <form action="mailto:renaldolouis555@gmail.com?subject=Customer Data"
+                            method="POST"
+                            enctype="text/plain"
+                            name="EmailForm"
+                            style={{ display: "grid" }}
+                        >
+                            <div>
+                                Email
+                            </div>
+                            <input style={{ marginBottom: "10px" }} type="text" placeholder="Email" name="Email" />
+                            <div>
+                                Messages
+                            </div>
+                            <textarea style={{ marginBottom: "10px" }} type="text" placeholder="Message" name="Message"></textarea>
+                            <button type="submit" value="Send" style={{ borderRadius: "0.25rem", padding: '6px', height: '100%' }}>Send</button>
+                        </form>
+                        <div className="seeAppButon">
+                            SEE APP
+                        </div>
                     </div>
-                    <div class="col">
-                        2 of 2
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        1 of 3
-                    </div>
-                    <div class="col">
-                        2 of 3
-                    </div>
-                    <div class="col">
-                        3 of 3
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col align-self-start">
-                        One of three columns
-                    </div>
-                    <div class="col align-self-center">
-                        One of three columns
-                    </div>
-                    <div class="col align-self-end">
-                        One of three columns
+                    <div class="col-5" style={{ alignSelf: "center" }}>
+                        or you can contact me through
+                        <div>
+                            <img src={linkedIn} alt="linkedin"></img>
+                            <img src={github} alt="github"></img>
+                            <img src={instagram} alt="instagram"></img>
+                        </div>
                     </div>
                 </div>
             </div>
